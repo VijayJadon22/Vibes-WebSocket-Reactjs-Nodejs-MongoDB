df18e89e7c06dc4b1ed9343a91b5b926ebac8443
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-25 23:59:48 +0530

    Initialized project

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import { connectToDB } from "./lib/db.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server started at PORT:${PORT}`);
+    connectToDB();
+});
