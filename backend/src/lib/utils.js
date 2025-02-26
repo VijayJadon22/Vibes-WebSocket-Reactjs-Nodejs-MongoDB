827e4a7cd42ca37658ea96fe04c44255f10ad1d4
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-26 12:43:44 +0530

    Implmented generateTokenAndSetCookie to generate token using jwt and setting cookie

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,12 @@
+// Function to generate a JWT token and set it as an HTTP-only cookie in the response
+export const generateTokenAndSetCookie = (user, res) => {
+    const token = user.getJWTToken(); // Get the JWT token from the user model instance
+
+    res.cookie("token", token, {
+        httpOnly: true, // The cookie is only accessible by the web server, not client-side JavaScript
+        secure: process.env.NODE_ENV !== "development", // The cookie is only sent over HTTPS in non-development environments
+        maxAge: 7 * 24 * 60 * 60 * 1000, // The cookie expires in 7 days (in milliseconds)
+        sameSite: "strict" // The cookie is not sent with requests initiated by third-party websites
+
+    })
+} 
