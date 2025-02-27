0fc9f07469263cc4529c6be87d4528746ea949f7
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-27 17:46:50 +0530

    Implemented auth updateProfile controller function for user to update profile imge with the help of cloudinary

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateTokenAndSetCookie } from "../lib/utils.js";
 import User from "../models/user.model.js";
 
@@ -84,6 +85,29 @@ export const logout = (req, res) => {
         return res.status(200).json({ message: "Logged out successfully" });
     } catch (error) {
         console.error("Error in logout controller: ", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic } = req.body;
+        const userId = req.user._id;
+
+        if (!profilePic) {
+            return res.status(400).json({ message: "Profile pic is required" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+        const updatedUser = await User.findByIdAndUpdate(userId,
+            { profilePic: uploadResponse.secure_url },
+            { new: true }
+        );
+
+        return res.status(200).json(updatedUser);
+    } catch (error) {
+        console.error("Error in updateProfile controller: ", error);
         return res.status(500).json({ error: "Internal server error" });
     }
 }
