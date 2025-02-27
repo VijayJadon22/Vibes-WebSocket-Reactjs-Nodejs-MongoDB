c0177d1fc9a9fed79220bbd0ad7dc68c1bdabdf6
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-27 19:36:17 +0530

    Added comments to the message model

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,24 +1,27 @@
 import mongoose from "mongoose";
 
+// Define the Message schema
 const messageSchema = new mongoose.Schema({
     senderId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User"
+        type: mongoose.Schema.Types.ObjectId, // Type is ObjectId
+        required: true, // Sender ID is required
+        ref: "User" // Reference the User model
     },
     receiverId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User"
+        type: mongoose.Schema.Types.ObjectId, // Type is ObjectId
+        required: true, // Receiver ID is required
+        ref: "User" // Reference the User model
     },
     text: {
-        type: String,
+        type: String, // Type is String
+        // Message text content (not required, can be empty)
     },
     image: {
-        type: String,
+        type: String, // Type is String
+        // URL of the image (not required, can be empty)
     }
+}, { timestamps: true }); // Add timestamps to track creation and update times
 
-}, { timestamps: true });
-
+// Create and export the Message model
 const Message = mongoose.model("Message", messageSchema);
 export default Message;
