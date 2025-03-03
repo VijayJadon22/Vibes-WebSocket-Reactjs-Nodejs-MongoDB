c05e65c124223430ae566ff42d44eba69f3afb10
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-03 18:30:20 +0530

    Implemented login auth functionality in store for logging in user

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -45,5 +45,19 @@ export const useAuthStore = create((set) => ({
             console.log("Error in logout: ", error);
             toast.error(error.response.data.message);
         }
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data });
+            toast.success("Logged in successfully");
+        } catch (error) {
+            console.log("Error in login: ", error);
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLoggingIn: false });
+        }
     }
 }))
