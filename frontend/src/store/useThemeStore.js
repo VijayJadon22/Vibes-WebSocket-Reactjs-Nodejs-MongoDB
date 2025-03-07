20a343646b1543dd0c9aefda7f7433b102e9e99c
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-03-07 16:47:09 +0530

    Created a useThemeStore custom hook to change theme and upload the current theme to localstorgae so that same theme can be applied even on refreshing

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,9 +1,16 @@
 import { create } from "zustand";
 
+// Create a Zustand store for theme management
 export const useThemeStore = create((set) => ({
+    // Initialize the theme state with the value from localStorage, or default to "dracula"
     theme: localStorage.getItem("chat-theme") || "dracula",
+
+    // Define a function to set a new theme
     setTheme: (theme) => {
-        localStorage.setItem("chat-theme", theme)
+        // Store the new theme value in localStorage
+        localStorage.setItem("chat-theme", theme);
+
+        // Update the theme state in the store
         set({ theme });
     }
 }));
